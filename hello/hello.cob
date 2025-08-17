e60a16caa1da3e9b7c707381d3c45d50be3a6ac1
Dj-Viking <anders.swedishviking@gmail.com>
2025-08-17 19:58:24 -0400

    source format

--- hello/hello.cob
+++ hello/hello.cob
@@ -1,6 +1,7 @@
-	      IDENTIFICATION DIVISION.
-	      PROGRAM-ID. hello.
-	      ENVIRONMENT DIVISION.
-	      PROCEDURE DIVISION.
-	         DISPLAY "Hello World!".
-		     STOP RUN.
+>>SOURCE FORMAT IS FREE
+          IDENTIFICATION DIVISION.
+          PROGRAM-ID. hello.
+          ENVIRONMENT DIVISION.
+          PROCEDURE DIVISION.
+             DISPLAY "Hello World!".
+             STOP RUN.
--- math/math.cob
+++ math/math.cob
@@ -1,13 +1,14 @@
-	      IDENTIFICATION DIVISION.
-	      PROGRAM-ID. math.
-	      ENVIRONMENT DIVISION.
-	      DATA DIVISION.
-	      WORKING-STORAGE SECTION.
+>>SOURCE FORMAT IS FREE
+          IDENTIFICATION DIVISION.
+          PROGRAM-ID. math.
+          ENVIRONMENT DIVISION.
+          DATA DIVISION.
+          WORKING-STORAGE SECTION.
           01 WS-VAR.
-		     *> single digit (01)
+             *> single digit (01)
              05 WS-A    PIC 9(01) VALUE 2.
              05 WS-B    PIC 9(01) VALUE 2.
-	      PROCEDURE DIVISION.
-		     ADD 0 WS-A TO WS-B.
-		     DISPLAY "Add result -> " WS-B. *> should display 4
-		     STOP RUN.
+          PROCEDURE DIVISION.
+             ADD 0 WS-A TO WS-B.
+             DISPLAY "Add result -> " WS-B. *> should display 4
+             STOP RUN.
