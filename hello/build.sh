31d50b732026aa36b43433981c42a9e033ee98f8
Dj-Viking <anders.swedishviking@gmail.com>
2025-08-17 19:36:26 -0400

    get hello project back

---
+++ hello/build.sh
@@ -0,0 +1,31 @@
+set -e
+# set -xe
+
+FILENAME=hello
+
+# clean up
+if [ -d "./dist" ]; then
+	if [ -z "./dist/$FILENAME" ]; then
+		rm "./dist/$FILENAME";
+	fi
+fi
+
+# make dist dir if not exist
+if ! [ -d "dist" ]; then
+	mkdir dist;
+	pushd dist;
+else
+	pushd dist;
+fi
+
+#compile and run
+cobc -x "../$FILENAME.cob" \
+	&& echo "" \
+	&& echo "running..." \
+	&& echo "" \
+	&& "./$FILENAME" \
+	&& echo "" \
+	&& echo "done..." \
+	&& echo "";
+
+popd
