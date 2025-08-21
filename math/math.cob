be1e7566fabef2c8df53ca8cae2132f98e96aa6c
Dj-Viking <anders.swedishviking@gmail.com>
2025-08-20 21:34:32 -0400

    update

--- math/math.cob
+++ math/math.cob
@@ -9,6 +9,6 @@
              05 WS-A    PIC 9(01) VALUE 2.
              05 WS-B    PIC 9(01) VALUE 2.
           PROCEDURE DIVISION.
-             ADD 0 WS-A TO WS-B.
+             ADD WS-A TO WS-B.
              DISPLAY "Add result -> " WS-B. *> should display 4
              STOP RUN.
