7230e27904beda4f9183e63da21cd6e05e929920
Dj-Viking <anders.swedishviking@gmail.com>
2025-08-20 21:41:06 -0400

    update folder name for multiple files to test ideas, update script to provide file name to compile and run

--- probe/build.sh
+++ probe/build.sh
@@ -1,7 +1,16 @@
-set -e
+
 # set -xe
 
-FILENAME=math
+set -e
+
+if [ -z "$1" ]; then
+	echo "[ERROR]: missing first arg to script 
+	 please provide name of file to compile and run
+	 e.g. 'sh build.sh math'"
+	exit 1;
+fi
+
+FILENAME="$1"
 
 # clean up
 if [ -d "./dist" ]; then
